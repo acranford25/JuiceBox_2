dc77243eaa835a6809b8dd045d3e0aaac47e9d98
Andrew Cranford <acranford25@gmail.com>
2023-05-21 08:42:44 -0400

    requireUser

--- api/posts.js
+++ api/posts.js
@@ -1,13 +1,40 @@
 const express = require("express");
 const postsRouter = express.Router();
+const { getAllPosts, createPost } = require("../db");
+const { requireUser } = require(`./utils`);
+
+postsRouter.post("/", requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+  const { id } = req.user;
+
+  const tagArr = tags.trim().split(/\s+/);
+  const postData = {};
+
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    postData.authorId = id;
+    postData.title = title;
+    postData.content = content;
+    const post = await createPost(postData);
+
+    if (post) {
+      res.send({ post });
+    } else {
+      next(error);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
   next();
 });
-
-const { getAllPosts } = require("../db");
 
 postsRouter.get("/", async (req, res) => {
   const posts = await getAllPosts();
